fc02e45157dd85944097e4da92023731e3e919e7
lifezq <lifezqy@126.com>
2021-11-08 20:12:08 +0800

    update

--- start.sh
+++ start.sh
@@ -2,3 +2,11 @@ minikube start --driver=docker --image-mirror-country='cn' --registry-mirror=htt
 #docker login --username=hi50091061@aliyun.com registry.cn-beijing.aliyuncs.com
 kubectl.exe expose deployment demo-deployment --port=80 --target-port=8000
 
+kubectl.exe apply -f nginx-deployment.yaml
+kubectl.exe logs --tail 100 -f deployment/nginx-deployment
+kubectl.exe get deployment/nginx-deployment
+kubectl.exe get po nginx-deployment-66b6c48dd5-24r6k
+kubectl.exe rollout status deployment/nginx-deployment
+kubectl.exe get deployments
+kubectl.exe get rs
+kubectl.exe get pods --show-labels
